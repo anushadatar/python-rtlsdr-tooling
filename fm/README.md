0842ec8ae97cc4aeaf0696565baf01a3041443cb
Anusha Datar <anushadatar@gmail.com>
2019-05-12 22:27:28 -0400

    added docs frameworks, mainly just TODOs

---
+++ fm/README.md
@@ -0,0 +1,5 @@
+# FM Demodulation
+
+# TODO Add an implementation of how this works
+# TODO Add a quick start guide
+# TODO Add TODOs
--- fm/play_fm.sh
+++ fm/play_fm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
 # Run the RTL-SDR Framework with the python script, and then play audio.
-rtl_sdr -f 88.9 -s 256k - | ./fm_demod.py -o | sox -t raw -r 256000 -b 16 -c 1 -L -e signed-integer - -d rate 32000
+rtl_sdr -f 92.9M -s 256k - | ./fm_demod.py -o | sox -t raw -r 256000 -b 16 -c 1 -L -e signed-integer - -d rate 32000
